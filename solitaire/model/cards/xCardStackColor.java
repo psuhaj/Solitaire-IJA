9f4e77e3e2b8f86535f97e3b00f3ce9a0df6b4a0
europ <adricko28@gmail.com>
2017-04-22 17:03:34 +0200

    bug fixed, stack color edit

--- solitaire/model/cards/xCardStackColor.java
+++ solitaire/model/cards/xCardStackColor.java
@@ -7,19 +7,18 @@ public class xCardStackColor extends xCardDeck {
 	private Card.Color color;
 	
 	// constructor
-	public xCardStackColor(Card.Color color) {
+	public xCardStackColor() {
 		this.stack      = new Stack<Card>();
 		this.stack_size = 0;
-		this.color      = color;
 	}
 
 	@Override
 	public boolean put(Card card) {
-		if (this.color != card.color()) return false; // card does not match package color
 		if (this.isEmpty()) { // is empty
 			if (1 == card.value()) { // is ace
 				this.stack_size++;
-				super.stack.push(card);
+				this.stack.push(card);
+				this.color = card.color();
 				return true;
 			}
 			else {
@@ -27,6 +26,7 @@ public class xCardStackColor extends xCardDeck {
 			}
 		}
 		else { // is not empty
+			if (this.color != card.color()) return false; // card does not match package color
 			int difference = this.stack_size + 1;
 			if (card.value() == difference) { // value is off by one
 				this.stack_size++;
