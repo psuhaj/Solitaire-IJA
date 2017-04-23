0a30c266beead645c0be981075a96495352f87e0
europ <adricko28@gmail.com>
2017-04-23 13:17:43 +0200

    ./run.sh - compile + run (not ant run)

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-if [ ! -f dest-client/ija-client.jar ]; then
-	ant compile
-	echo
-fi
+#if [ ! -f dest-client/ija-client.jar ]; then
+#	ant compile
+#	echo
+#fi
 
+ant compile
 java -jar dest-client/ija-client.jar
