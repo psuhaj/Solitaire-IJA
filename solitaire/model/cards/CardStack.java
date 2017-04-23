e52cbe7431ff2ddff10b08361b6a410df7f7246a
peter2141 <pupet2141@gmail.com>
2017-04-23 18:50:54 +0200

    deleted shit

--- solitaire/model/cards/CardStack.java
+++
@@ -1,9 +0,0 @@
-package solitaire.model.cards;
-
-public interface CardStack extends CardDeck {
-
-	public boolean put(CardStack stack);
-        public boolean putEmpty(Card card);
-	public CardStack pop(Card card);
-
-}
