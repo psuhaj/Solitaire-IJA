dfd6efa0861a29efd12e30a4a33c94f420b9b0ae
Adrian <adricko28@gmail.com>
2017-04-26 20:30:20 +0200

    wget error

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -4,5 +4,11 @@ destination="lib"
 file1="cards"
 
 wget -q "http://www.stud.fit.vutbr.cz/~xtotha01/$file1.zip" -P $destination
+
+if [ $? -ne 0 ]; then
+	printf "ERROR: Could not download \"cards.zip\"!\n"
+	exit
+fi
+
 unzip -q $destination/$file1.zip -d $destination
 rm -f $destination/$file1.zip
