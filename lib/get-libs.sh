7218180d4d484002f2b01c262c5c4c45bdab4a7c
europ <adricko28@gmail.com>
2017-06-20 19:17:16 +0200

    Update get-libs.sh

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -17,7 +17,7 @@ if [ -d "$destination/$file1" ]; then
     fi
 fi
 
-wget -q "https://github.com/europ/stuff/raw/master/cards.zip" -P $destination > /dev/null 2>&1
+wget -q "https://github.com/europ/Solitaire-images/raw/master/cards.zip" -P $destination > /dev/null 2>&1
 if [ $? -ne 0 ]; then Error "ERROR: Could not download \"cards.zip\"!\n" 1; fi
 
 unzip -q $destination/$file1.zip -d $destination > /dev/null 2>&1
